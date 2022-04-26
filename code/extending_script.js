4a2131f2e9dad3c54923b17d946a99d502050cf3
Strangereal1 <K.D.vs.Melo@Gmail.com>
2022-04-27 02:37:22 +0800

    fix: add comment

--- code/extending_script.js
+++ code/extending_script.js
@@ -4,7 +4,22 @@ window.onload = () => {
 
 const models = [
   {
-    url: './assets/myModel/scene.gltf',
+    url: './assets/myModel/1.glb',
+    scale: '0.5 0.5 0.5',
+    rotation: '0 90 0'
+  },
+  {
+    url: './assets/myModel/2.glb',
+    scale: '0.5 0.5 0.5',
+    rotation: '0 180 0'
+  },
+  {
+    url: './assets/myModel/3.glb',
+    scale: '0.5 0.5 0.5',
+    rotation: '0 270 0'
+  },
+  {
+    url: './assets/myModel/4.glb',
     scale: '0.5 0.5 0.5',
     rotation: '0 225 0'
   },
--- code/script.js
+++ code/script.js
@@ -1,62 +1,68 @@
 window.onload = () => {
   let places = staticLoadPlaces();
   renderPlaces(places);
+  // When the page loads, the function staticLoadPlaces is executed, and the return value places is passed to the function renderPlaces for rendering.
 };
 
 function staticLoadPlaces() {
+  // a function staticLoadPlaces returns a list of latitude and longitude.
   return [
     {
       name: "Magnemite1",
       location: {
-        lat: 44.49647,
-        lng: 11.32018,
+        // location
+        lat: 40.49647, // latitude
+        lng: 116.32018, // longitude
       },
     },
-    {
-      name: "Magnemite2",
-      location: {
-        lat: 39.91092455,
-        lng: 116.4133837,
-      },
-    },
-    {
-      name: "Magnemite3",
-      location: {
-        lat: 40.072820063960634,
-        lng: 116.318075997769877,
-      },
-    },
-    {
-      name: "Magnemite4",
-      location: {
-        lat: 40.0728,
-        lng: 116.318,
-      },
-    },
+    // {
+    //   name: "Magnemite2",
+    //   location: {
+    //     lat: 39.91092455,
+    //     lng: 116.4133837,
+    //   },
+    // },
+    // {
+    //   name: "Magnemite3",
+    //   location: {
+    //     lat: 40.072820063960634,
+    //     lng: 116.318075997769877,
+    //   },
+    // },
+    // {
+    //   name: "Magnemite4",
+    //   location: {
+    //     lat: 40.0728,
+    //     lng: 116.318,
+    //   },
+    // },
   ];
 }
 
 function renderPlaces(places) {
-  let scene = document.querySelector("a-scene");
-
+  // a function to render the model
+  let scene = document.querySelector("a-scene"); // get the element with the tag "a-scene"
   places.forEach((place) => {
-    let latitude = place.location.lat;
-    let longitude = place.location.lng;
+    // traverse the entire places array
+    let latitude = place.location.lat; // get latitude
+    let longitude = place.location.lng; // get longitude
 
-    let model = document.createElement("a-entity");
+    let model = document.createElement("a-entity"); // create an "a-entity" element name "model"
     model.setAttribute(
+      // assign latitude and longitude properties to "model" and declare that this is a "gps-entity-place"
       "gps-entity-place",
       `latitude: ${latitude}; longitude: ${longitude};`
     );
-    model.setAttribute("gltf-model", "./assets/magnemite/scene.gltf");
-    model.setAttribute("rotation", "0 180 0");
-    model.setAttribute("animation-mixer", "");
-    model.setAttribute("scale", "0.005 0.005 0.005");
-
+    model.setAttribute("gltf-model", "./assets/magnemite/scene.gltf"); // Set the "gltf-model" property
+    model.setAttribute("rotation", "0 180 0"); // Set the "rotation" property
+    model.setAttribute("animation-mixer", ""); // Set the "animation-mixer" property
+    model.setAttribute("scale", "0.005 0.005 0.005"); // Set the "scale" property
+    a;
     model.addEventListener("loaded", () => {
+      // Add listener event to window dispatch event "gps-entity-place-loaded"
       window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
     });
 
-    scene.appendChild(model);
+    scene.appendChild(model); // Add the model to the scene
   });
 }
