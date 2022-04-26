1aa0fe7473eee50ad0d6b3470745ca4f38ebef10
Strangereal1 <K.D.vs.Melo@Gmail.com>
2022-04-27 02:56:44 +0800

    Update extending_script.js

--- code/extending_script.js
+++ code/extending_script.js
@@ -4,49 +4,51 @@ window.onload = () => {
 
 const models = [
   {
-    url: '../assets/myModel/9.glb',
-    scale: '0.5 0.5 0.5',
-    rotation: '0 90 0'
+    url: "../assets/myModel/9.glb",
+    scale: "0.5 0.5 0.5",
+    rotation: "0 90 0",
   },
   {
-    url: '../assets/myModel/10.glb',
-    scale: '0.5 0.5 0.5',
-    rotation: '0 180 0'
+    url: "../assets/myModel/10.glb",
+    scale: "0.5 0.5 0.5",
+    rotation: "0 180 0",
   },
   {
-    url: '../assets/myModel/11.glb',
-    scale: '0.5 0.5 0.5',
-    rotation: '0 270 0'
+    url: "../assets/myModel/11.glb",
+    scale: "0.5 0.5 0.5",
+    rotation: "0 270 0",
   },
   {
-    url: '../assets/myModel/12.glb',
-    scale: '0.5 0.5 0.5',
-    rotation: '0 225 0'
+    url: "../assets/myModel/12.glb",
+    scale: "0.5 0.5 0.5",
+    rotation: "0 225 0",
   },
 ];
 
 let modelIndex = 0;
 const setModel = (model, entity) => {
   if (model.position) {
-    entity.setAttribute('position', model.position);
+    entity.setAttribute("position", model.position);
   }
 
-  entity.setAttribute('gltf-model', model.url);
+  entity.setAttribute("gltf-model", model.url);
 };
 
 function render() {
-  const scene = document.querySelector('a-scene');
-
+  const scene = document.querySelector("a-scene");
   navigator.geolocation.getCurrentPosition(function (position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
-    const model = document.createElement('a-entity');
-    model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    const model = document.createElement("a-entity");
+    model.setAttribute(
+      "gps-entity-place",
+      `latitude: ${latitude}; longitude: ${longitude};`
+    );
 
     setModel(models[modelIndex], model);
 
-    model.setAttribute('animation-mixer', '');
+    model.setAttribute("animation-mixer", "");
 
     scene.appendChild(model);
   });
