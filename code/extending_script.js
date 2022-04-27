dabfee4834fff73a8454b8135b0d6ca4b0603e24
Strangereal1 <K.D.vs.Melo@Gmail.com>
2022-04-27 23:31:49 +0800

    Update extending_script.js

--- code/extending_script.js
+++ code/extending_script.js
@@ -13,19 +13,19 @@ const models = [
     url: "assets/magnemite/scene.gltf",
     scale: "0.01 0.01 0.01",
     rotation: "0 90 0",
-    position: "3 0 3",
+    position: "30 0 30",
   },
   {
     url: "assets/magnemite/scene.gltf",
     scale: "0.01 0.01 0.01",
     rotation: "0 180 0",
-    position: "3 3 0",
+    position: "30 30 0",
   },
   {
     url: "assets/magnemite/scene.gltf",
     scale: "0.01 0.01 0.01",
     rotation: "0 270 0",
-    position: "0 3 3",
+    position: "0 30 30",
   },
 ];
 
@@ -44,13 +44,16 @@ function render() {
     navigator.geolocation.getCurrentPosition(function (position) {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      const model = document.createElement("a-entity");
+      const model = document.createElement("a-box");
       model.setAttribute(
         "gps-entity-place",
-        `latitude: ${latitude}; longitude: ${longitude};`
+        `latitude: ${latitude}; longitude: ${longitude};`,
+        `position: ${item.position}`,
+        `rotation: ${item.rotation}`,
+        `scale: ${item.scale}`
       );
 
-      setModel(item, model);
+    //   setModel(item, model);
 
       model.setAttribute("animation-mixer", "");
       scene.appendChild(model);
