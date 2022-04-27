b01b3170571630f844040fcdec0ae1517dcdc868
Strangereal1 <K.D.vs.Melo@Gmail.com>
2022-04-28 01:05:48 +0800

    Update extending_script.js
    change psition

--- code/extending_script.js
+++ code/extending_script.js
@@ -37,19 +37,28 @@ const models = [
 function render() {
   const scene = document.querySelector("a-scene");
 
-  models.map((item) => {
+  models.map((item,index) => {
     navigator.geolocation.getCurrentPosition(function (position) {
       const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+      const longitude = position.coords.longitude + index;
       const model = document.createElement("a-enity");
       model.setAttribute(
         "gps-entity-place",
         `latitude: ${latitude}; longitude: ${longitude};`
       );
-      model.setAttribute("position", item.position);
+	//   if (item.position){
+    //   model.setAttribute("position", item.position);
+	//   }
+	  if(item.scale){
       model.setAttribute("scale", item.scale);
+	  }
+	  if (item.rotation){
       model.setAttribute("rotation", item.rotation);
+	  }
+	  if (item.color){
       model.setAttribute("color", item.color);
+	  }
+
       model.setAttribute("gltf-model", item.url);
 
       model.setAttribute("animation-mixer", "");
