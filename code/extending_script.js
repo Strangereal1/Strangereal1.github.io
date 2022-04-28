94ba08544d81ff5d1f12f0ef623a87a2c6eeaa01
Strangereal1 <K.D.vs.Melo@Gmail.com>
2022-04-28 21:32:08 +0800

    1 version

--- code/extending_script.js
+++ code/extending_script.js
@@ -5,65 +5,71 @@ window.onload = () => {
 const models = [
   {
     url: "assets/magnemite/scene.gltf",
-	scale: "0.05 0.05 0.05",
+    scale: "0.1 0.1 0.1",
     rotation: "0 0 0",
-    position: "0",
+    position: "0 -10 0",
     color: "yellow",
   },
   {
     url: "assets/magnemite/scene.gltf",
-	scale: "0.05 0.05 0.05",
+    scale: "0.005 0.01 0.01",
     rotation: "0 90 0",
-    position: "1",
+    position: "0 -10 0",
     color: "red",
   },
   {
     url: "assets/magnemite/scene.gltf",
-	scale: "0.05 0.05 0.05",
-    rotation: "0 180 0",
-    position: "2",
+    scale: "0.05 0.05 0.05",
+    rotation: "0 270 0",
+    position: "0 5 0",
     color: "blue",
   },
   {
     url: "assets/magnemite/scene.gltf",
     scale: "0.05 0.05 0.05",
     rotation: "0 270 0",
-    position: "3",
+    position: "0 10 0",
     color: "green",
   },
 ];
 
+// let modelIndex = 0;
+const setModel = (model, entity) => {
+  if (model.position) {
+    entity.setAttribute("position", model.position);
+  }
 
+  //   if (model.scale) {
+  //     entity.setAttribute("scale", model.scale);
+  //   }
+
+  entity.setAttribute("gltf-model", model.url);
+  //   entity.setAttribute("color", model.color);
+  entity.setAttribute("rotation", model.rotation);
+};
+
+const modelIndex = 0;
 function render() {
   const scene = document.querySelector("a-scene");
 
-  models.map((item) => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-      const model = document.createElement("a-enity");
-      model.setAttribute(
-        "gps-entity-place",
-        `latitude: ${latitude}; longitude: ${longitude};`
-      );
-	//   if (item.position){
-    //   model.setAttribute("position", item.position);
-	//   }
-	  if(item.scale){
-      model.setAttribute("scale", item.scale);
-	  }
-	  if (item.rotation){
-      model.setAttribute("rotation", item.rotation);
-	  }
-	  if (item.color){
-      model.setAttribute("color", item.color);
-	  }
+  navigator.geolocation.getCurrentPosition(function (position) {
+    // const latitude = position.coords.latitude;
+    // const longitude = position.coords.longitude;
+    const latitude = 40.065662;
+    const longitude = 116.311533;
 
-      model.setAttribute("gltf-model", item.url);
+    const model = document.createElement("a-enity");
+    model.setAttribute(
+      "gps-entity-place",
+      `latitude: ${latitude}; longitude: ${longitude};`
+    );
 
-      model.setAttribute("animation-mixer", "");
-      scene.appendChild(model);
-      console.log("%O", model);
-    });
+    setModel(models[modelIndex], model);
+
+    console.log(model);
+
+    model.setAttribute("animation-mixer", "");
+
+    scene.appendChild(model);
   });
 }
